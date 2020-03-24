813fb930fa1924363f3f3c0b631bbcf434f2e02a
Isa <isamartrujillo1999@gmail.com>
2020-03-24 09:15:08 +0000

    Initial data

--- index.js
+++ index.js
@@ -1,10 +1,87 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 
 var app = express();
 
 var port = process.env.PORT || 80;
 
+app.use(bodyParser.json());
 app.use("/", express.static("./public"));
+
+const BASE_API_URL = "/api/v1";
+	
+	// INITIAL DATA
+
+var vehicles = [
+	{
+		province:"Almería",
+		year: 2018,
+		car: 361888,
+		bus: 831,
+		motorcycle: 44574,
+		truck: 96518,
+		total: 533067
+		
+	},
+	{
+		province:"Las Palmas",
+		year: 2017,
+		car: 570714,
+		bus: 2949,
+		motorcycle: 62944,
+		truck: 180984,
+		total: 839149
+	}
+];
+
+var roads = [
+	{
+		province:"Almería",
+		year: 2018,
+		oneway: 2003,
+		multipleway: 4,
+		dualcarriagewaysandhighway: 321,
+		highwaywithtoll: 28,
+		total: 2356
+	},
+	{
+		province:"Las Palmas",
+		year: 2017,
+		oneway: 1962,
+		multipleway: 76,
+		dualcarriagewaysandhighway: 129,
+		highwaywithtoll: 0,
+		total: 2149
+	}
+];
+
+var traffic_accidents = [
+	{
+		province:"Almería",
+		year: 2018,
+		accidentwithvictims: 1194,
+		mortalaccident: 27,
+		death: 27,
+		hospitalizedwounded: 111,
+		nothospitalizedwounded: 1780
+	},
+	{
+		province:"Las Palmas",
+		year: 2017,
+		accidentwithvictims: 1302,
+		mortalaccident: 29,
+		death: 30,
+		hospitalizedwounded: 115,
+		nothospitalizedwounded: 1886
+	}
+];
+
+	// GET VEHICLES
+
+app.get(BASE_API_URL+"/vehicles/loadInitialData", (req,res) =>{
+	res.send(JSON.stringify(vehicles,null,2));
+	console.log("Data sent:"+JSON.stringify(vehicles,null));
+});
 
 app.listen(port, () => {
 		console.log("server ready");
