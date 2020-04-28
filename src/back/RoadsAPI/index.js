f8d24d2d0ac55610d608b0d070404df5f01fb4c3
Isa <isamartrujillo1999@gmail.com>
2020-04-28 16:39:37 +0200

    error

--- src/back/RoadsAPI/index.js
+++ src/back/RoadsAPI/index.js
@@ -3,7 +3,7 @@ module.exports = function(app){
 	const dataStore = require("nedb");
 	const path = require("path");
 	
-	const dbFileName = path.join(__dirname, "RoadsAPI.json");
+	const dbFileName = path.join(__dirname, "RoadsAPI.db");
 	const BASE_API_URL = "/api/v1";
 	
 	const db = new dataStore({
