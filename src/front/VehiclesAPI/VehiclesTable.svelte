35318f239874463ce58d420520320fda14c6153e
Isa <isamartrujillo1999@gmail.com>
2020-05-03 11:41:16 +0200

    searches

--- src/front/VehiclesAPI/VehiclesTable.svelte
+++ src/front/VehiclesAPI/VehiclesTable.svelte
@@ -6,10 +6,19 @@
 	import Table from "sveltestrap/src/Table.svelte";
 	import Button from "sveltestrap/src/Button.svelte";
 
-	export let params = {};
 	let offset = 0;
 	let vehicles = [];
 	let newVehicle = {
+		province: "",
+		year: "",
+		car: "",
+		bus: "",
+		motorcycle: "",
+		truck: "",
+		total: ""
+	};
+
+	let searchVehicle = {
 		province: "",
 		year: "",
 		car: "",
@@ -40,26 +49,29 @@
 
 	async function insertVehicle() {
 
-		newVehicle.year = parseInt(newVehicle.year);
-		newVehicle.car = parseInt(newVehicle.car);
-		newVehicle.bus = parseInt(newVehicle.bus);
-		newVehicle.motorcycle = parseInt(newVehicle.motorcycle);
-		newVehicle.truck = parseInt(newVehicle.truck);
-		newVehicle.total = parseInt(newVehicle.total);
+		let vehicle = {
+			province: newVehicle.province,
+			year: parseInt(newVehicle.year),
+			car: parseInt(newVehicle.car),
+			bus: parseInt(newVehicle.bus),
+			motorcycle: parseInt(newVehicle.motorcycle),
+			truck: parseInt(newVehicle.truck),
+			total: parseInt(newVehicle.total)
+		};
 
 		console.log("Inserting vehicle..." + JSON.stringify(newVehicle));
 
-		if (isNaN(newVehicle.year) || isNaN(newVehicle.car) || isNaN(newVehicle.bus) || isNaN(newVehicle.motorcycle)
-		|| isNaN(newVehicle.truck) || isNaN(newVehicle.total)){
-			errorMsg = "Alguno de los valores introducidos no es numérico.";
+		if (isNaN(vehicle.year) || isNaN(vehicle.car) || isNaN(vehicle.bus) || isNaN(vehicle.motorcycle)
+		|| isNaN(vehicle.truck) || isNaN(vehicle.total)){
+			errorMsg = "Alguno de los atributos no es numérico";
 			successMsg = false;
 		}else{
-			const dataBaseGet = await fetch("/api/v1/vehicles/" + newVehicle.province +"/"+ newVehicle.year);
+			const dataBaseGet = await fetch("/api/v1/vehicles/" + vehicle.province +"/"+ vehicle.year);
 			if (!dataBaseGet.ok){
 				errorMsg = false;
 				const res = await fetch("/api/v1/vehicles", {
 					method: "POST",
-					body: JSON.stringify(newVehicle),
+					body: JSON.stringify(vehicle),
 					headers: {
 						"Content-Type": "application/json"
 					}
@@ -115,11 +127,56 @@
 	};
 
 	async function anteriorPagina() {
-		if (offset -10 >= 0){
+		if (offset - 10 >= 0){
 			offset = offset - 10;
 			getVehicles(offset);
 		}
 	};
+
+	async function searchVehicles(offset) {
+		let url = "/api/v1/vehicles?limit=10&offset="+ offset;
+		console.log("Searching vehicles...");
+
+		let vehicle = {
+			province: searchVehicle.province,
+			year: parseInt(searchVehicle.year),
+			car: parseInt(searchVehicle.car),
+			bus: parseInt(searchVehicle.bus),
+			motorcycle: parseInt(searchVehicle.motorcycle),
+			truck: parseInt(searchVehicle.truck),
+			total: parseInt(searchVehicle.total)
+		};
+
+		Object.entries(vehicle).forEach(([x,y]) => {
+			if(!isNaN(y)){
+				url = url + "&" + x + "=" + y;
+			}
+		});
+		if(!vehicle.province == ""){
+			url = url +"&province=" + vehicle.province;
+		};
+		
+		const res = await fetch(url);
+
+		if (res.ok) {
+			console.log("Ok:");
+			const json = await res.json();
+			vehicles = json;
+			console.log("Received " + vehicles.length + " vehicles.");
+			if (vehicles.length > 0){
+				successMsg = "Se ha realizado la búsqueda.";
+				errorMsg = false;
+			}else{
+				successMsg = false;
+				errorMsg = "La búsqueda no ha obtenido resultados.";
+			}
+			
+		} else {
+			console.log("ERROR!");
+			successMsg = false;
+			errorMsg = "La búsqueda no ha obtenido resultados.";
+		}
+	}
 
 </script>
 
@@ -143,14 +200,14 @@
 			</thead>
 			<tbody>
 				<tr>
-					<td><input bind:value="{newVehicle.province}"></td>
-					<td><input bind:value="{newVehicle.year}"></td>
-					<td><input bind:value="{newVehicle.car}"></td>
-					<td><input bind:value="{newVehicle.bus}"></td>
-					<td><input bind:value="{newVehicle.motorcycle}"></td>
-					<td><input bind:value="{newVehicle.truck}"></td>
-					<td><input bind:value="{newVehicle.total}"></td>
-					<td> <Button outline  color="primary" on:click={insertVehicle}>Insertar</Button> </td>
+					<td><input bind:value="{searchVehicle.province}"></td>
+					<td><input bind:value="{searchVehicle.year}"></td>
+					<td><input bind:value="{searchVehicle.car}"></td>
+					<td><input bind:value="{searchVehicle.bus}"></td>
+					<td><input bind:value="{searchVehicle.motorcycle}"></td>
+					<td><input bind:value="{searchVehicle.truck}"></td>
+					<td><input bind:value="{searchVehicle.total}"></td>
+					<td> <Button outline  color="primary" on:click={searchVehicles}>Buscar</Button> </td>
 				</tr>
 
 				{#each vehicles as vehicle}
@@ -167,6 +224,16 @@
 						<td><Button outline color="danger" on:click="{deleteVehicle(vehicle.province,vehicle.year)}">Eliminar</Button></td>
 					</tr>
 				{/each}
+				<tr>
+					<td><input bind:value="{newVehicle.province}"></td>
+					<td><input bind:value="{newVehicle.year}"></td>
+					<td><input bind:value="{newVehicle.car}"></td>
+					<td><input bind:value="{newVehicle.bus}"></td>
+					<td><input bind:value="{newVehicle.motorcycle}"></td>
+					<td><input bind:value="{newVehicle.truck}"></td>
+					<td><input bind:value="{newVehicle.total}"></td>
+					<td> <Button outline  color="primary" on:click={insertVehicle}>Insertar</Button> </td>
+				</tr>
 			</tbody>
 		</Table>
 	{/await}
