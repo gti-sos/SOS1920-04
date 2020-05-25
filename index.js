994333065924f6307eef77c1ec930c2d3edf6175
Isa <isamartrujillo1999@gmail.com>
2020-05-25 11:48:05 +0200

    cors

--- index.js
+++ index.js
@@ -9,14 +9,7 @@ const cors = require("cors");
 const port = process.env.PORT || 1234;
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-}) 
-//app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 
 backVehicles(app);
