36630f938f9f76760f98dda82e177d0d812d66ea
manubarreram18 <manuelbarreramoya@yahoo.es>
2020-06-01 19:08:15 +0200

    video barrera

--- src/front/about/about.svelte
+++ src/front/about/about.svelte
@@ -7,4 +7,8 @@
 <p>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/Ul3TDY09LUc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </p>
+<h3>Vídeo de la API de Accidentes de tráfico, realizado por Manuel Barrera Moya</h3>
+<p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/uWa3wXKvWQM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</p>
 </main>
