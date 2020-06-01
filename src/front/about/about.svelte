29e4ec95117107c17f50874a5e8acbe9afcfa2c8
Isa <isamartrujillo1999@gmail.com>
2020-06-01 09:31:13 +0200

    video

---
+++ src/front/about/about.svelte
@@ -0,0 +1,6 @@
+<main>
+<h3>Vídeo de la API de Vehicles, realizado por Isabel María Trujillo Gómez</h3>
+<p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/tssKjLuKhbI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</p>
+</main>
