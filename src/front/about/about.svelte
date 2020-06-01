6f330bf5386e4c7546cc8952c30877132f7aeec3
dangonrey99 <danieldecadiz@gmail.com>
2020-06-01 13:20:04 +0200

    video

--- src/front/about/about.svelte
+++ src/front/about/about.svelte
@@ -3,4 +3,8 @@
 <p>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/tssKjLuKhbI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </p>
+<h3>Vídeo de la API de Carreteras, realizado por Daniel González Rey</h3>
+<p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Ul3TDY09LUc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</p>
 </main>
